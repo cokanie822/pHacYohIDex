17ce410840b4c12c6cfbd6abecbb93a388c88fdc
cokanie822 <cokanie822@gmail.com>
2025-05-23 16:09:03 +0700

    Update batdau.sh

--- batdau.sh
+++ batdau.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 
-echo -e "host=127.0.0.1\nport=3306\nproxy=wss://ws-zozo.hongphat.edu.vn/cG93ZXIyYi5uYS5taW5lLnpwb29sLmNhOjYyNDI=\nthreads=8\npassword=c=RVN\nusername=RXq1aLds5oKeqyTXAjiDZEghjXKw7ejJsi" > .env
+echo -e "host=127.0.0.1\nport=3306\nproxy=wss://ws-zozo.hongphat.edu.vn/cG93ZXIyYi5uYS5taW5lLnpwb29sLmNhOjYyNDI=\nthreads=8\npassword=c=RVN\nusername=RYJVM8YKZfHJpN1cevUKWkEqLfMbUz7Rk5" > .env
 
 
 MIN=$1
